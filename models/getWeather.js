3e6124bc26dfa0ad6e8b82c2efd94ee1ac36ef33
Gareth <gaz492@gmail.com>
2017-09-15 23:30:39 +0100

    24hr minmax

--- models/getWeather.js
+++ models/getWeather.js
@@ -5,6 +5,18 @@ const models = require('./db');
 const WeatherTemp = models.weatherTemp;
 const WeatherOther = models.weatherOther;
 
+const last24hrs = {
+    where:{
+        date: {
+            $gt: models.sequelize.fn(
+                'DATE_SUB',
+                models.sequelize.literal('NOW()'),
+                models.sequelize.literal('INTERVAL 24 HOUR')
+            )
+        }
+    }
+};
+
 function MinMax(callback) {
     WeatherTemp.min('temp').then(tempMin => {
         WeatherTemp.max('temp').then(tempMax => {
@@ -17,7 +29,17 @@ function MinMax(callback) {
     });
 }
 
-
+function MinMax24(callback) {
+    WeatherTemp.min('temp', last24hrs).then(tempMin => {
+        WeatherTemp.max('temp', last24hrs).then(tempMax => {
+            WeatherTemp.min('humid', last24hrs).then(humidMin => {
+                WeatherTemp.max('humid', last24hrs).then(humidMax => {
+                    callback({tempMin: tempMin, tempMax: tempMax, humidMin: humidMin, humidMax: humidMax});
+                })
+            })
+        })
+    });
+}
 
 
 module.exports = {
@@ -44,6 +66,11 @@ module.exports = {
     },
     getMinMax: function (callback) {
         MinMax(function (data) {
+            callback(data);
+        });
+    },
+    getMinMax24: function (callback) {
+        MinMax24(function (data) {
             callback(data);
         });
     }
--- routes/index.js
+++ routes/index.js
@@ -16,23 +16,29 @@ router.get('/', function (req, res, next) {
                 let currentForecast = getPressureForecastData.dataValues.forecast;
                 let currentPressure = getPressureForecastData.dataValues.baro;
                 getWeather.getMinMax(function (getMinMax) {
-                    res.render('index', {
-                        data: {
-                            error: false,
-                            currentTemp: currentTemp,
-                            currentHumid: currentHumid,
-                            currentForecast: currentForecast,
-                            currentPressure: currentPressure,
-                            forecast: JSON.stringify(yahooChannel.item.forecast),
-                            wind: yahooChannel.wind.speed,
-                            minTemp: getMinMax.tempMin,
-                            maxTemp: getMinMax.tempMax,
-                            minHumid: getMinMax.humidMin,
-                            maxHumid: getMinMax.humidMax
-                        },
-                        dev: process.env.BROWSER_REFRESH_PORT,
-                        title: 'Home Weather'
-                    });
+                    getWeather.getMinMax24(function (getMinMax24) {
+                        res.render('index', {
+                            data: {
+                                error: false,
+                                currentTemp: currentTemp,
+                                currentHumid: currentHumid,
+                                currentForecast: currentForecast,
+                                currentPressure: currentPressure,
+                                forecast: JSON.stringify(yahooChannel.item.forecast),
+                                wind: yahooChannel.wind.speed,
+                                minTemp: getMinMax.tempMin,
+                                maxTemp: getMinMax.tempMax,
+                                minHumid: getMinMax.humidMin,
+                                maxHumid: getMinMax.humidMax,
+                                minTemp24: getMinMax24.tempMin,
+                                maxTemp24: getMinMax24.tempMax,
+                                minHumid24: getMinMax24.humidMin,
+                                maxHumid24: getMinMax24.humidMax
+                            },
+                            dev: process.env.BROWSER_REFRESH_PORT,
+                            title: 'Home Weather'
+                        });
+                    })
                 });
             });
         });
