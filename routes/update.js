825106868fd31ff9d803ddb110b9d93c92b5ff12
Unknown <gaz492@gmail.com>
2017-09-18 19:05:02 +0100

    debug

--- routes/update.js
+++ routes/update.js
@@ -10,28 +10,6 @@ const models = require('../models/db');
 const WeatherTemp = models.weatherTemp;
 const WeatherOther = models.weatherOther;
 
-/*function test(callback) {
-    WeatherOther.findAll({
-    }).then((data) => {
-        callback(data)
-    }).catch((err) => {
-        console.log(err);
-        callback("NaN")
-    })
-}
-
-function test2() {
-    WeatherOther.create({
-        temp: 99.9
-    }).complete(function (err, data) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(data)
-        }
-    })
-}*/
-
 /* GET home page. */
 router.post('/', function (req, res, next) {
 
@@ -69,12 +47,14 @@ router.post('/', function (req, res, next) {
         }
         else {
             res.header("Content-Type", "application/json");
-            res.status(401).json({code: 401, response: "Error: Unauthorized Request", data: JSON.stringify(postData)})
+            res.status(401).json({code: 401, response: "Error: Unauthorized Request", data: JSON.stringify(postData)});
+            console.log("Error: " + JSON.stringify(postData))
         }
     }
     else {
         res.header("Content-Type", "application/json");
-        res.status(401).json({code: 401, response: "Error: Unauthorized Request", data: JSON.stringify(postData)})
+        res.status(401).json({code: 401, response: "Error: Unauthorized Request", data: JSON.stringify(postData)});
+        console.log("Error: " + JSON.stringify(postData))
     }
 });
 
